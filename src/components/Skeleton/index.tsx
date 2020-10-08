4bd9617c230e6c74cfb96e20765fdaf8731e16b1
Fabio Bandacheski <fabio.versao.beta@gmail.com>
2020-10-08 20:16:46 -0300

    skeleton conflict

--- src/components/Skeleton/index.tsx
+++ src/components/Skeleton/index.tsx
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import styled from 'styled-components';
-
-export default styled.div`
-  background-image: linear-gradient(
-    -90deg,
-    #e7edf1 0%,
-    #f8f8f8 50%,
-    #e7edf1 100%
-  );
-  background-size: 400% 400%;
-  animation: shimmer 1.2s ease-in-out infinite;
-
-  @keyframes shimmer {
-    0% {
-      background-position: 0% 0%;
-    }
-    100% {
-      background-position: -135% 0%;
-    }
-  }
-
-  &.white {
-    background-image: linear-gradient(-90deg, #fff 0%, #e7edf1 50%, #fff 100%);
-  }
-`;
-=======
 import styled from 'styled-components';
 
 export default styled.div`
@@ -55,4 +28,3 @@ export default styled.div`
         );
     }
 `;
->>>>>>> video-entrypoint
